d34bd21445423cacdf712921682df93430c2ce4c
Varsha Kushwah <varshakushwah711@gmail.com>
2025-03-28 23:22:10 +0530

    Create insert_customers.sql

---
+++ e-commerce management system/insert_customers.sql
@@ -0,0 +1,6 @@
+INSERT INTO Customers(cust_name) VALUES
+('Varsha kushwah'),('Tripti patel'),('Prerna sharma'), 
+('Kanita verma'),('Saloni Rajput'),('Mansi chouhan'),
+('Shivani mathe'),('Diksha verma'),('Renuka jain'),
+('Sonali choudhary'),('Rahul sharma'),
+('Deepak sen'),('Shital bercha');
